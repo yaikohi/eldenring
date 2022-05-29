60b84df50532685837194f5215c87cf18c2f9f13
yaikohi <erik.beem@hotmail.com>
2022-05-29 16:47:03 +0200

    add two endpoints ammo & armor

---
+++ pages/api/ammo.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import ammoData from '../../public/data/ammo.json'
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): void {
+  const { name } = req.query
+
+  if (!name) {
+    return res
+      .status(400)
+      .send('you must provide a name in order to find an ammo piece')
+  }
+
+  const findAmmo = ammoData.find((ammo) => ammo.name === req.query.name)
+
+  res.setHeader('Content-Type', 'application/json')
+
+  if (!findAmmo) {
+    return res.status(400).send(`ammo '${name}' not found.`)
+  }
+
+  return res.status(200).send(JSON.stringify(findAmmo))
+}
---
+++ pages/api/armor.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import armorData from '../../public/data/armor.json'
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): void {
+  const { name } = req.query
+
+  if (!name) {
+    return res
+      .status(400)
+      .send('you must provide a name in order to find an armor piece')
+  }
+
+  const findArmor = armorData.find((armor) => armor.name === req.query.name)
+
+  res.setHeader('Content-Type', 'application/json')
+
+  if (!findArmor) {
+    return res.status(400).send(`Armor '${name}' not found.`)
+  }
+
+  return res.status(200).send(JSON.stringify(findArmor))
+}
