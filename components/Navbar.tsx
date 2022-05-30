d07da321c403ddfc4d872bdee994158840f45f88
Erik Beem <erik.beem@hotmail.com>
2022-05-30 21:03:03 +0200

    Navbar created

---
+++ components/Navbar.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link'
+
+type Props = {}
+const links = [
+  { href: '/', name: 'Home' },
+  { href: 'ammo', name: 'Ammo' },
+  { href: 'armor', name: 'Armor' },
+  { href: 'ashes', name: 'Ashes' },
+  { href: 'bosses', name: 'Bosses' },
+  { href: 'classes', name: 'Classes' },
+  { href: 'creatures', name: 'Creatures' },
+  { href: 'incantations', name: 'Incantations' },
+  { href: 'items', name: 'Items' },
+  { href: 'locations', name: 'Locations' },
+  { href: 'npcs', name: 'Npcs' },
+  { href: 'shields', name: 'Shields' },
+  { href: 'sorceries', name: 'Sorceries' },
+  { href: 'spirits', name: 'Spirits' },
+  { href: 'talismans', name: 'Talismans' },
+  { href: 'weapons', name: 'Weapons' },
+]
+
+export const Navbar = ({}: Props) => {
+  return (
+    <>
+      <ul className="flex p-4 bg-slate-900 text-yellow-50">
+        {links.map((link, index) => {
+          return (
+            <li key={index} className="ml-3 first:ml-0">
+              <Link href={`/${link.href}`}>
+                <a className="capitalize"> {link.name} </a>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
 import type { NextPage } from 'next'
 import { Card } from '../components/Card'
+import { Navbar } from '../components/Navbar'
 import ashes from '../public/data/ashes.json'
 
 const Home: NextPage = () => {
   return (
     <>
-      <div className="flex flex-col gap-10">
-        {ashes.map((ash, index) => {
-          return <Card key={index} item={ash} />
-        })}
-      </div>
+      <Navbar />
     </>
   )
 }
