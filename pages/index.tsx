23a130055dd3721c0d3d66412c11f5d8200829d9
Erik Beem <erik.beem@hotmail.com>
2022-05-31 11:32:30 +0200

    homepage Add text

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,9 @@ import ashes from '../public/data/ashes.json'
 const Home: NextPage = () => {
   return (
     <>
+      <div className="flex flex-col p-4 ">
+        <h1 className="text-3xl">Home page</h1>
+      </div>
       <Navbar />
     </>
   )
