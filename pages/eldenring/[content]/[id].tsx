481caf7382acfc754506c6cd807f792be83e0eb1
yaikohi <erik.beem@hotmail.com>
2022-06-02 16:00:55 +0200

    getServerSideProps WIP, add [id] route

---
+++ pages/eldenring/[content]/[id].tsx
@@ -0,0 +1,72 @@
+import type {
+  NextPage,
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+} from 'next'
+import { Card } from 'components/Card'
+import { Navbar } from 'components/Navbar'
+import { EldenRingContent } from 'types/union'
+
+const URL = 'https://eldenring.fanapis.com/api/'
+
+export const getServerSideProps: GetServerSideProps = async (
+  context,
+) => {
+  const { params } = context
+  console.log(params)
+
+  if (params !== undefined) {
+    const res = await fetch(`${URL}${params.content}/${params.id}`)
+    const resJson = await res.json()
+    const data = resJson.data
+
+    if (data !== undefined) {
+
+      return {
+        props: {
+          data,
+        },
+      }
+    } else {
+      const data = null
+      return {
+        props: {
+          data
+        }
+      }
+    }
+  } else {
+    const data = null
+    return {
+      props: {
+        data,
+      },
+    }
+  }
+}
+
+const ContentById: NextPage<EldenRingContent[]> = ({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  if (data !== null) {
+
+
+    return (
+      <>
+        <Navbar />
+        <div className="flex flex-col gap-10">
+          <Card item={data} />
+        </div>
+      </>
+    )
+  } else {
+    return (<>
+      <Navbar />
+      <div className="flex flex-col gap-10">
+        Card
+      </div>
+    </>)
+  }
+}
+
+export default ContentById
