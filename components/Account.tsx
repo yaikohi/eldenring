718a019814aa050d64fa2694a378b047efd28666
yaikohi <erik.beem@hotmail.com>
2022-06-22 13:25:41 +0200

    Update profile styling, add .env locally

--- components/Account.tsx
+++ components/Account.tsx
@@ -79,63 +79,65 @@ export default function Account({ session }: unknown | any) {
   }
 
   return (
-    <div className="flex flex-col">
-      {/* Input + label EMAIL */}
-      <div className="bg-black ">
-        <label className="text-sm text-white" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="w-full px-3 py-2 font-sans text-base transition ease-in-out rounded-lg disabled:bg-slate-500 disabled:text-white focus:text-slate-500 focus:bg-white focus:border-yellow-900 focus:outline-none"
-          id="email"
-          type="text"
-          value={session.user.email}
-          disabled
-        />
-      </div>
-      {/* Input + label USERNAME */}
-      <div className="bg-black">
-        <label className="text-sm text-white" htmlFor="username">
-          Username
-        </label>
-        <input
-          className="w-full px-3 py-2 font-sans text-base transition ease-in-out rounded-lg disabled:bg-slate-500 disabled:text-white focus:text-slate-500 focus:bg-white focus:border-yellow-900 focus:outline-none"
-          id="username"
-          type="text"
-          value={username || ''}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      {/* Input + label WEBSITE*/}
-      <div className="bg-black ">
-        <label className="text-sm text-white" htmlFor="website">
-          Website
-        </label>
-        <input
-          className="w-full px-3 py-2 font-sans text-base transition ease-in-out rounded-lg disabled:bg-slate-500 disabled:text-white focus:text-slate-500 focus:bg-white focus:border-yellow-900 focus:outline-none"
-          id="website"
-          type="website"
-          value={website || ''}
-          onChange={(e) => setWebsite(e.target.value)}
-        />
-      </div>
+    <div className="w-full max-w-sm px-4 bg-black">
+      <div className="flex flex-col min-w-full ">
+        {/* Input + label EMAIL */}
+        <div className="flex flex-col py-2 overflow-x-scroll ">
+          <label className="px-4 text-sm text-white" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="px-3 py-2 font-sans text-base transition ease-in-out rounded-lg disabled:-500 disabled:text-white focus:text-slate-500 focus: focus:border-yellow-900 focus:outline-none"
+            id="email"
+            type="text"
+            value={session.user.email}
+            disabled
+          />
+        </div>
+        {/* Input + label USERNAME */}
+        <div className="flex flex-col py-2 overflow-x-scroll ">
+          <label className="px-4 text-sm text-white" htmlFor="username">
+            Username
+          </label>
+          <input
+            className="px-3 py-2 font-sans text-base transition ease-in-out rounded-lg disabled:-500 disabled:text-white focus:text-slate-500 focus: focus:border-yellow-900 focus:outline-none"
+            id="username"
+            type="text"
+            value={username || ''}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        {/* Input + label WEBSITE*/}
+        <div className="flex flex-col py-2 overflow-x-scroll ">
+          <label className="px-4 text-sm text-white" htmlFor="website">
+            Website
+          </label>
+          <input
+            className="px-3 py-2 font-sans text-base transition ease-in-out rounded-lg disabled:-500 disabled:text-white focus:text-slate-500 focus: focus:border-yellow-900 focus:outline-none"
+            id="website"
+            type="website"
+            value={website || ''}
+            onChange={(e) => setWebsite(e.target.value)}
+          />
+        </div>
 
-      <div>
-        <button
-          className=""
-          onClick={() =>
-            updateProfile({ username, website, avatar_url })
-          }
-          disabled={loading}
-        >
-          {loading ? 'Loading ...' : 'Update'}
-        </button>
-      </div>
+        <div className="p-2 text-white rounded-xl ">
+          <button
+            className=""
+            onClick={() =>
+              updateProfile({ username, website, avatar_url })
+            }
+            disabled={loading}
+          >
+            {loading ? 'Loading ...' : 'Update'}
+          </button>
+        </div>
 
-      <div>
-        <button className="" onClick={() => supabase.auth.signOut()}>
-          Sign Out
-        </button>
+        <div className="p-2 text-white rounded-xl ">
+          <button className="" onClick={() => supabase.auth.signOut()}>
+            Sign Out
+          </button>
+        </div>
       </div>
     </div>
   )
